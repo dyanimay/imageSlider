9469663b96766ed19564e11019bd8909e06a3278
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-25 16:34:40 +0200

    now css works but the pictures are not found...

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('assets'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// home page
+app.use(express.static('assets'));
+
 app.get('/', (req, res) => {
-  // render `home.ejs`
-  res.render('home', {
-    title: 'Hello World!',
-  });
+  res.render('home', { pictureList });
 });
 
+//this should be in another home.js file because that would be more front-end
+const pictureList = [
+  {url: "uj1.jpg", title: "The first version", description: "it is really long"},
+]
 // start express app on port 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
