5634b0df44381dee093c44562cb065cd1abd50bb
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-25 15:46:13 +0200

    started to add the js and host

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const cocktails = [
+  { title: 'the first picture', link: "../valami"},
+];
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+    res.render('home', {
+      todos
+    });
+  }
+)
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
